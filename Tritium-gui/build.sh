b09094102cb8aa5faab8cd5a29ed08bdf18749c5
Michalis Pelekanos <m.pelekanos@gmail.com>
2019-02-08 21:00:32 +0200

    additions

--- Tritium-gui/build.sh
+++ Tritium-gui/build.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-docker build -t tritium-gui -f Dockerfile-tritium-gui-ubuntu-18.04 .
+set -e
+
+NEXUS_GUI_VERSION=merging
+
+[[ $(id -u) -eq 0 ]] && {
+UNAME=nexus
+DUID=1342
+DGID=1342
+}
+
+[[ $(id -u) -ne 0 ]] && {
+UNAME=$(id -un)
+DUID=$(id -u)
+DGID=$(id -g)
+}
+
+docker build -t tritium-gui \
+--build-arg UNAME=${UNAME} \
+--build-arg DUID=${DUID} \
+--build-arg DGID=${DGID} \
+-f Dockerfile-tritium-gui-ubuntu-18.04 .
