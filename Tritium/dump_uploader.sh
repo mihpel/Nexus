7a7ed8322a0300ba42f3e850dd46f3cd5f1c9e87
Michalis Pelekanos <m.pelekanos@gmail.com>
2019-02-09 00:00:48 +0200

    path fix

--- Tritium/dump_uploader.sh
+++ Tritium/dump_uploader.sh
@@ -15,7 +15,10 @@ echo "OAUTH_ACCESS_TOKEN=${OAUTH_ACCESS_TOKEN}" > ${HOME}/.dropbox_uploader
 for i in \
 compiled_version.txt \
 debug.log \
-$(ls -1 ~/.TAO/ | grep core)
+$(ls -1 ~/.TAO/ | grep ^core)
 do \
+[[ -f ~/.TAO/${i} ]] && {
 bash dropbox_uploader.sh upload ~/.TAO/${i} dumps/${uploadtime}-${i}
+rm ~/.TAO/${i}
+}
 done
