7760fc9a8ef02f03c70a05b047d024c23fb97f7d
Michalis Pelekanos <michalis@25-8projects.com>
2019-02-09 12:43:26 +0200

    reset data dir

---
+++ Tritium/reset-data.sh
@@ -0,0 +1,10 @@
+bash tritium.sh stop
+
+find ${HOME}/.TAO \
+-mindepth 1 \
+! -regex "${HOME}/.TAO/LLL-TAO\(/.*\)?" \
+! -regex "${HOME}/.TAO/compiled_version.txt\(/.*\)?" \
+! -regex "${HOME}/.TAO/wallet.dat\(/.*\)?" \
+-delete
+
+bash tritium.sh start
