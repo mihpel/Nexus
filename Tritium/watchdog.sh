773cc55455e8764dd74c6912ddb3c7e036bbb901
Michalis Pelekanos <m.pelekanos@gmail.com>
2019-02-09 15:35:02 +0200

    more visual indicators

--- Tritium/watchdog.sh
+++ Tritium/watchdog.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+commits=0
+fails=0
 while : ;do
+echo "rebuilds since mointoring begun : ${commits}"
+echo "fails since mointoring begun : ${fails}"
 date +%y-%m-%d-%H:%M:%S
 git pull
 [[ "$(cat ~/.TAO/compiled_version.txt | grep ^commit | awk '{print $2}')" \
@@ -8,12 +12,14 @@ bash +x tritium.sh rebuild-code
 bash +x tritium.sh stop
 bash +x reset-data.sh
 bash +x tritium.sh start
+(( commits +=1 ))
 }
 docker ps | grep nexus
 [[ $? -ne 0 ]] && {
 bash +x dump_uploader.sh
 bash +x reset-data.sh
 bash +x tritium.sh start
+(( fails +=1 ))
 }
 sleep 600
 done
