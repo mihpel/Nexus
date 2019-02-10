19a82cc351eabfed9c5531739928e13f54d47466
Michalis Pelekanos <m.pelekanos@gmail.com>
2019-02-10 18:03:06 +0200

    single check per loop

--- Tritium/log-monitor-loop.sh
+++ Tritium/log-monitor-loop.sh
@@ -1,8 +1,11 @@
-while : ;do
-[[ -z $(docker ps | grep tritium-code) ]] && {
-#clear
-echo -e "\nWaitting for container with\nname:'nexus'\nand\nimage:'tritium-code'\nto become ready!"
-sleep 5
-}
-[[ -z $(docker ps | grep tritium-code) && -z $(docker ps | grep nexus) ]] || docker logs -f nexus
+while :
+do
+	if [[ -z $(docker ps | grep tritium-code) && -z $(docker ps | grep nexus) ]]
+	then
+#		clear
+		echo -e "\nWaitting for container with\nname:'nexus'\nand\nimage:'tritium-code'\nto become ready!"
+		sleep 5
+	else
+		docker logs -f nexus
+	fi
 done
